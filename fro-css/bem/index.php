be2f8327dd266562dd6039769b6edd52e6d18c7f
Justin Fox | Emile Vos <36817937+JustinFox404@users.noreply.github.com>
2018-10-01 14:54:57 +0200

    Added BEM index.php

---
+++ fro-css/bem/index.php
@@ -0,0 +1,160 @@
+
+<section class="page-header">
+    <h1 class="text-center color-white heading1 hr-margin text-uppercase">"We recraeted<br>the internet"</h1>
+</section>
+<section class="page-what-we-offer bg-primary">
+    <div class="container">
+        <h1 class="text-center color-black heading1 hr-margin text-uppercase">What we offer</h1>
+        <hr class="bg-white">
+        <div class="page-what-we-offer__wrapper">
+            <div class="page-what-we-offer__wrapper__offer">
+                <h2 class="hr-margin text-uppercase">Webhosting</h2>
+                <hr>
+                <div class="page-what-we-offer__wrapper__offer__item">
+                    <div class="icon"><i class="fas fa-location-arrow"></i></div>
+                    <a href="#">Domain names</a>
+                    <span>From 8.00 EUR/j</span>
+                </div>
+                <div class="page-what-we-offer__wrapper__offer__item">
+                    <div class="icon"><i class="fas fa-globe-americas"></i></div>
+                    <a href="#">Webhosting</a>
+                    <span>From 5.00 EUR/m</span>
+                </div>
+                <div class="page-what-we-offer__wrapper--item">
+                    <div class="icon"><i class="fas fa-fingerprint"></i></div>
+                    <a href="#">SSL Certificates</a>
+                    <span>From 5.00 EUR/j</span>
+                </div>
+                <div class="kickstarter page-what-we-offer__wrapper__item">
+                    <div class="icon"><i class="fas fa-balance-scale"></i></div>
+                    <a href="#">Web Load Balancers</a>
+                    <span>To Kickstarter</span>
+                </div>
+                <div class="kickstarter page-what-we-offer__wrapper__item">
+                    <div class="page-what-we-offer__wrapper__offer--icon"><i class="fas fa-battery-full"></i></div>
+                    <a href="#">Dedicated Webhosting</a>
+                    <span>To Kickstarter</span>
+                </div>
+            </div>
+            <div class="page-what-we-offer__wrapper__offer">
+                <h2>Dedicated</h2>
+                <hr>
+                <div class="kickstarter page-what-we-offer__wrapper__item">
+                    <div class="icon"><i class="fas fa-server"></i></div>
+                    <a href="#">Traditional Servers</a>
+                    <span>To Kickstarter</span>
+                </div>
+                <div class="kickstarter page-what-we-offer__wrapper__item">
+                    <div class="icon"><i class="fas fa-gamepad"></i></div>
+                    <a href="#">Game Servers</a>
+                    <span>To Kickstarter</span>
+                </div>
+                <div class="kickstarter page-what-we-offer__wrapper__item">
+                    <div class="icon"><i class="fas fa-hdd"></i></div>
+                    <a href="#">Storage Servers</a>
+                    <span>To Kickstarter</span>
+                </div>
+                <div class="kickstarter page-what-we-offer__wrapper__item">
+                    <div class="page-what-we-offer__wrapper__offer__item--icon"><i class="fas fa-stream"></i></div>
+                    <a href="#">GPU Servers</a>
+                    <span>To Kickstarter</span>
+                </div>
+            </div>
+            <div class="page-what-we-offer__wrapper__offer">
+                <h2>Cloud</h2>
+                <hr>
+                <div class="page-what-we-offer__wrapper__item">
+                    <div class="icon"><i class="fas fa-database"></i></div>
+                    <a href="#">Database</a>
+                    <span>From 8.00 EUR/j</span>
+                </div>
+                <div class="kickstarter page-what-we-offer__wrapper__item">
+                    <div class="icon"><i class="fas fa-memory"></i></div>
+                    <a href="#">VPS</a>
+                    <span>To Kickstarter</span>
+                </div>
+                <div class="kickstarter page-what-we-offer__wrapper__item">
+                    <div class="icon"><i class="fas fa-hdd"></i></div>
+                    <a href="#">Storage</a>
+                    <span>To Kickstarter</span>
+                </div>
+                <div class="kickstarter page-what-we-offer__wrapper__item">
+                    <div class="icon"><i class="fas fa-database"></i></div>
+                    <a href="#">HIGH AVAILABILITY</a>
+                    <span>To Kickstarter</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="page-why-us">
+    <div class="container">
+        <h1 class="heading1 text-black text-center">Why us?</h1>
+        <hr><br>
+        <h2 class="text-center text-uppercase">This is what we are</h2>
+        <div class="page-why-us__points">
+            <div class="page-why-us__points__item">
+                <div class="icon"><i class="fas fa-rocket"></i></div>
+                <span>Fast and reliable</span>
+                <p>The slogan of Foxiko is create awesome things. An service that is not fast or reliable is not awesome, it's stupid.</p>
+            </div>
+            <div class="page-why-us__points__item">
+                <div class="icon"><i class="fas fa-fingerprint"></i></div>
+                <span>Private and secure</span>
+                <p>As a company we care about our customers thats why we would do anything in our power to protect and secure our costumers information..</p>
+            </div>
+            <div class="page-why-us__points__item">
+                <div class="icon"><i class="fas fa-code"></i></div>
+                <span>Innovative</span>
+                <p>Our company is constantly moving we want to create new awesome things as well as providing the best user expirence as possible.</p>
+            </div>
+            <div class="page-why-us__points__item">
+                <div class="icon"><i class="fas fa-test"></i></div>
+                <span>EMPTY</span>
+                <p>N/A</p>
+            </div>
+        </div>
+        <div class="page-why-us__mindset text-center">
+            <h2 class="text-uppercase hr-margin">Our mindset</h2>
+            <hr>
+            <p>Foxiko Web Services is one of the largest upcoming web services company, because we have a customer first business plan instead of a company first business plan can we great support and great prices.</p>
+        </div>
+        <div class="page-why-us-checklist">
+            <h2 class="text-uppercase text-center hr-margin">Our checklist</h2>
+            <hr>
+            <div class="page-why-us__checklist__list">
+                <div>
+                    <div class="page-why-us__checklist__list__item"><div class="icon"><i class="fas fa-check"></i></div><span>costomer oriented</span></div>
+                    <div class="page-why-us__checklist__list__item"><div class="icon"><i class="fas fa-check"></i></div><span>easy to use</span></div>
+                    <div class="page-why-us__checklist__list__item"><div class="icon"><i class="fas fa-check"></i></div><span>enterprise hardware</span></div>
+                    <div class="page-why-us__checklist__list__item"><div class="icon"><i class="fas fa-check"></i></div><span>scaleable</span></div>
+                </div>
+                <div>
+                    <div class="page-why-us__checklist__list__item"><div class="icon"><i class="fas fa-check"></i></div><span>full control</span></div>
+                    <div class="page-why-us__checklist__list__item"><div class="icon"><i class="fas fa-check"></i></div><span>professional team</span></div>
+                    <div class="page-why-us__checklist__list__item"><div class="icon"><i class="fas fa-check"></i></div><span>secure</span></div>
+                    <div class="page-why-us__checklist__list__item"><div class="icon"><i class="fas fa-check"></i></div><span>reliable</span></div>
+                </div>
+                <div>
+                    <div class="page-why-us__checklist__list__item"><div class="icon"><i class="fas fa-check"></i></div><span>no nonsense</span></div>
+                    <div class="page-why-us__checklist__list__item"><div class="icon"><i class="fas fa-check"></i></div><span>custom made</span></div>
+                    <div class="page-why-us__checklist__list__item"><div class="icon"><i class="fas fa-check"></i></div><span>fast support</span></div>
+                    <div class="page-why-us__checklist__list__item"><div class="icon"><i class="fas fa-check"></i></div><span>N/A</span></div>
+                </div>
+            </div>
+        </div>
+        <div class="page-why-us__support">
+            <div class="page-why-us__support__title">
+                <div class="page-why-us-support__title__text"><h1>Our groundbreaking support</h1></div>
+                <hr>
+            </div>
+            <p>We have a 24/7 support team thats allways there when you need it. We have the greatest problemsolving minds set on the support team. We will always try to solve the problem you are having, if it is inside or outside our networks, it doesn't matter. If the problem is created within our service.</p>
+        </div>
+    </div>
+</section>
+<section class="page-who-are-we-for bg-primary">
+    <div class="container">
+        <h1 class="text-center color-black heading1 hr-margin text-uppercase">Who are we for?</h1>
+        <hr class="bg-white">
+    </div>
+</section>
