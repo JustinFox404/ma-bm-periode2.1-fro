c0ae3b65985d4fad994f25cf48e6155299487b7c
Justin Fox | Emile Vos <36817937+JustinFox404@users.noreply.github.com>
2018-10-10 12:02:05 +0200

    Added project Foundation and project Bootstrap

---
+++ fro-css/bootstrap/index.php
@@ -0,0 +1,187 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: emile
+ * Date: 10/9/2018
+ * Time: 8:46 AM
+ */
+
+/*
+ *
+ *  Copyright (c) 2018, Foxiko B.V. i.o. and/or its affiliates. ALL RIGHTS RESERVED.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *    - Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *
+ *    - Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *
+ *     - Neither the name of Foxiko or the names of its
+ *      contributors may be used to endorse or promote products derived
+ *      from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ *  IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ *  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ *  PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ *  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ *  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ *  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ *  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ *
+ *
+ */
+?>
+<!--
+ *
+ *  Copyright (c) 2018, Foxiko B.V. i.o. and/or its affiliates. ALL RIGHTS RESERVED.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *    - Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *
+ *    - Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *
+ *     - Neither the name of Foxiko or the names of its
+ *      contributors may be used to endorse or promote products derived
+ *      from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ *  IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ *  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ *  PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ *  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ *  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ *  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ *  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ *
+ *
+-->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>THIS IS THE FOCKING NEWS</title>
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+    <script src="node_modules/jquery/dist/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="fontawesome-pro/js/fontawesome.min.js"></script>
+</head>
+<body>
+<header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">News</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Account
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="#">My account</a>
+                            <a class="dropdown-item" href="#">Delete my account</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item bg-danger text-white" href="#">Logout</a>
+                        </div>
+                    </li>
+                </ul>
+                <form class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit"><i class="far fa-search"></i></button>
+                </form>
+            </div>
+        </div>
+    </nav>
+</header>
+<div class="container" style="margin-top: 1rem;">
+    <article>
+        <figure>
+            <h1>Uitspraak in klimaatzaak: moet de overheid meer doen tegen CO2-uitstoot?</h1>
+            <p class="text-bol"><i class="fal fa-clock"></i> VANDAAG, 8:55</p>
+            <img src="https://nos.nl/data/image/2018/10/09/505245/xxl.jpg">
+        </figure>
+        <p>Vandaag doet het gerechtshof in Den Haag uitspraak in de klimaat-zaak van actiegroep Urgenda. Vraag is of de overheid meer moet doen om de uitstoot van CO2 terug te dringen. De rechter bepaalde drie jaar geleden in deze zaak dat de Staat de CO2-uitstoot in 2020 met 25 procent moet verlagen, ten opzichte van 1990. De zaak werd historisch genoemd en leidde tot flink wat discussie.</p>
+        <p>De rechter ging met de uitspraak te veel op de stoel van de overheid zitten, vond de Staat, die in hoger beroep ging. Niet omdat klimaatveranderingen niet serieus worden genomen, benadrukte de advocaat meerdere malen tijdens zijn pleidooi. "Maar omdat de rechtbank een beleidsmatige en politieke keuze heeft gemaakt." De rechter hoort dat volgens hem niet te doen. Tegelijk beloofde het kabinet het vonnis wel te zullen uitvoeren.</p>
+        <h3>Leefbaarheid voor (klein)kinderen</h3>
+        <p>Dat is ook nodig, zegt Sietse Brouwer van Grootouders voor het Klimaat. "Als het zo doorgaat komt de voedselvoorziening in het geding door droogte en wateroverlast." Samen met andere grootouders maakt hij zich net als Urgenda sterk voor leefbaarheid van de wereld voor zijn kleinkinderen. "Daarom staan wij twee keer per maand bij de Tweede Kamer. Daar praten we met Kamerleden en bewindspersonen om hen aan te sporen om spoed te maken met goed klimaatbeleid, want dat is echt heel hard nodig."</p>
+        <p>Hij vindt dat die boodschap in Den Haag nog niet goed genoeg aankomt. "En hoe sneller we ingrijpen, hoe beperkter de gevolgen zullen zijn. De politiek moet dus sneller acteren." Over de snelheid waarmee dat nu gebeurt, of het gebrek daaraan, maakt hij zich zorgen.</p>
+        <p>"Maar ik ben ook optimistisch. Want er zijn oplossingen voorhanden: bedrijven moeten acteren, de politiek moet de wettelijke kaders plaatsen en ook de consument moet aan de slag." Dat betekent volgens Brouwer niet dat het leven er minder leuk op wordt, vertelt hij in het NOS Radio 1 Journaal. "Dat kan echt op een hele leuke manier. Je hoeft jezelf ook niet alles te ontzeggen, maar hoe sneller we gedragsverandering doorzetten, des te leuker het blijft."</p>
+        <h3>Uitdaging</h3>
+        <p>Op dit moment staat het kabinet voor een flinke uitdaging. Het CBS maakte een maand geleden bekend dat de totale uitstoot aan broeikasgassen in 2017 slechts 13 procent lager was dan in 1990. Volgens het vonnis van drie jaar geleden moet de uitstoot in 2020 een kwart lager zijn. Als het hof de eerdere uitspraak verwerpt hoeft de Staat dit doel niet wettelijk na te komen.</p>
+        <strong>De uitspraak is vanaf 10.00 uur live te volgen op NPO Nieuws en NOS.nl</strong>
+        <a target="_blank" href="https://nos.nl/artikel/2254013-uitspraak-in-klimaatzaak-moet-de-overheid-meer-doen-tegen-co2-uitstoot.html">Content van NOS.nl</a>
+        <br><br>
+    </article>
+</div>
+<footer>
+    <div class="jumbotron bg-primary text-white text-center" style="margin-bottom: 0;">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 col-md-6 col-lg-3">
+                    <i class="fal fa-paper-plane fa-4x"></i>
+                    <h1>Contact</h1>
+                    <p><strong>Foxiko B.V. i.o.</strong><br>Goudplaat 58<br>2134WK Hoofddorp<br>Nederland</p>
+                    <a href="mailto:support@foxiko.com" class="btn btn-secondary"><i class="fal fa-envelope"></i></a>
+                    <a href="https://twitter.com/" class="btn btn-secondary"><i class="fab fa-twitter"></i></a>
+                    <a href="https://facebook.com/" class="btn btn-secondary"><i class="fab fa-facebook-f"></i></a>
+                    <br><br>
+                </div>
+                <div class="col-sm-12 col-md-6 col-lg-3">
+                    <i class="fal fa-pen-fancy fa-4x"></i>
+                    <h1>Content van NOS.nl</h1>
+                    <a href="https://nos.nl/artikel/2254013-uitspraak-in-klimaatzaak-moet-de-overheid-meer-doen-tegen-co2-uitstoot.html" class="btn btn-secondary">Ga naar</a>
+                    <br><br>
+                </div>
+                <div class="col-sm-12 col-md-6 col-lg-3">
+                    <i class="fal fa-pen-fancy fa-4x"></i>
+                    <h1>Class aptent</h1>
+                    <p>dolor sit amet, consectetur adipiscing elit. Etiam cursus mi eget condimentum tincidunt.</p>
+                    <a href="#" class="btn btn-secondary">Ga naar</a>
+                    <br><br>
+                </div>
+                <div class="col-sm-12 col-md-6 col-lg-3">
+                    <i class="fal fa-pen-fancy fa-4x"></i>
+                    <h1>Lorem ipsum</h1>
+                    <p>taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
+                    <a href="#" class="btn btn-secondary">Ga naar</a>
+                    <br><br>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="jumbotron bg-dark text-white text-center" >Copyright &copy; <strong>Foxiko B.V. i.o.</strong> 2018 ALL RIGHTS RESERVED</div>
+</footer>
+</body>
+</html>
+
---
+++ fro-css/foundation/index.php
@@ -0,0 +1,80 @@
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Foundation for Sites</title>
+    <link rel="stylesheet" href="css/foundation.css">
+    <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="font-awesome-pro/css/all.min.css">
+    <script src="font-awesome-pro/js/all.js"></script>
+</head>
+<body>
+<div class="reveal full" id="modal1" data-reveal style="width: 100%; height: 100%;">
+    <div class="image" style="width: 90%; height: 90%;">
+        <img src="https://nos.nl/data/image/2018/10/02/503713/xxl.jpg" style="width: 90%; height: 90%;">
+        <button class="button" data-close aria-label="Close modal" type="button">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+</div>
+<div class="grid-container">
+    <ul class="menu grid-x ">
+        <li class="cell small-12 medium-4 large-auto"><a>Logo</a></li>
+        <li class="cell small-12 medium-4 large-auto active"><a>Home</a></li>
+        <li class="cell small-12 medium-4 large-auto"><a>News</a></li>
+        <li class="cell small-12 medium-4 large-auto"><a>Shop</a></li>
+        <li class="cell small-12 medium-4 large-auto"><a>Hello</a></li>
+        <li class="cell small-12 medium-4 large-auto"><a>About</a></li>
+        <li class="cell small-12 medium-4 large-auto">
+            <ul class="dropdown menu" data-dropdown-menu>
+                <li>
+                    <a href="#">Account</a>
+                    <ul class="menu">
+                        <li><a href="#">Login</a></li>
+                        <li><a href="#">My account</a></li>
+                        <li><a href="#">Change password</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </li>
+        <li class="cell small-12 medium-4 large-3">
+            <div class="input-group">
+                <input class="input-group-field" type="text">
+                <div class="input-group-button">
+                    <button type="submit" class="button"><i class="fal fa-search"></i></button>
+                </div>
+            </div>
+        </li>
+    </ul>
+</div>
+<div class="grid-container">
+    <section>
+        <h1>Invallen en arrestaties in Utrecht, Vianen, Nieuwegein en Tiel</h1>
+        <span><i class="fal fa-clock"></i>VANDAAG, 07:36</span>
+    </section>
+    <section>
+        <div class="image">
+            <img src="https://nos.nl/data/image/2018/10/02/503713/xxl.jpg">
+            <button class="button" data-open="modal1"><i class="fas fa-search-plus"></i></button>
+        </div>
+    </section>
+    <section>
+        <p>In Nieuwegein, Utrecht, Tiel en Vianen heeft de politie vanmorgen op acht plekken invallen gedaan. Zeven mensen zijn daarbij gearresteerd. Op dit moment vinden nog verschillende doorzoekingen plaats.</p>
+        <p>De politie wil alleen zeggen dat de invallen te maken hebben met "een lopend onderzoek".</p>
+        <p>Volgens justitie-redacteur Remco Andringa heeft de operatie te maken met een vete in de onderwereld, die de afgelopen jaren heeft geleid tot een lange reeks liquidaties. De politie houdt daarom zoekacties in verschillende woningen en opslagplaatsen. "Daar zijn ze op zoek naar zaken die bij liquidaties worden gebruikt, zoals bijvoorbeeld snelle auto's en zware wapens", zegt Andringa.</p>
+        <p>Het onderzoek houdt ook verband met Ridouan T, die ervan wordt verdacht als opdrachtgever betrokken te zijn geweest bij verschillende liquidaties. "Er wordt zelfs rekening gehouden met meer dan tien moorden", aldus Andringa.</p>
+        <p>"Iemand uit de kring van T. is een tijdje geleden overgelopen naar de politie en heeft daar een boekje open gedaan over onder meer de activiteiten van T. Vlak daarna werd de onschuldige broer van deze overgelopen man doodgeschoten, kennelijk als vergelding", aldus Andringa. "Dat geeft wel aan dat het gaat om zeer zware criminelen die niet terugdeinzen voor een moord meer of minder."</p>
+
+        <p>T. is zelf is vanmorgen niet aangehouden. Bronnen melden dat er wel invallen zijn gedaan bij familie van hem. T. is al maanden spoorloos en wordt internationaal gezocht. Voor zijn aanhouding is een beloning uitgeloofd van 25.000 euro.</p>
+        <p>Ridouan T. groeide op in Vianen en is volgens het OM actief in "een crimineel milieu waarin heel snel wordt overgegaan tot geweld". Het OM heeft niet bekendgemaakt aan welke liquidaties T. wordt gelinkt. Op misdaadsites wordt zijn naam in verband gebracht met moorden die werden uitgevoerd door een groep criminelen uit Nieuwegein.</p>
+    </section>
+</div>
+
+<script src="js/vendor/jquery.js"></script>
+<script src="js/vendor/what-input.js"></script>
+<script src="js/vendor/foundation.js"></script>
+<script src="js/app.js"></script>
+</body>
+</html>
